6b3c7a7ad2ece143821ee2d6b0af11d50a43737a
Ramdeep Kesharwani <106538883+ram8528@users.noreply.github.com>
2024-11-16 19:08:53 +0000

    17-11-2024

---
+++ 17-11-2024.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseArray(int arr[]) {
+        int start =0, end = arr.length-1;
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            
+            start++;
+            end--;
+        }
+    }
+}
